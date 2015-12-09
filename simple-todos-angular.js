6f084209d7c65968441599da6517ca1872e9de6c
ImmaDragon <devindewitt8@gmail.com>
2015-12-09 18:07:22 -0500

    hide completed tasks not working

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -2,21 +2,57 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
 
-  angular.module('simple-todos',['angular-meteor']);
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
+  angular.module('simple-todos',['angular-meteor', 'accounts.ui']);
+
+
+// cordova bitch
+  function onReady() {
+    angular.bootstrap(document, ['simple-todos']);
+  }
+
+  if (Meteor.isCordova)
+    angular.element(document).on('deviceready', onReady);
+  else
+    angular.element(document).ready(onReady);
+//
 
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope','$meteor',
     function ($scope, $meteor) {
 
-      $scope.tasks = $meteor.collection(function(){
-        return Tasks.find({},{sort: {createdAt: -1}})
+      //turn angular scope varaibles into meteor reactive variables
+      $scope.tasks = $meteor.collection(function() {
+        return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
       });
+
+      //create a scope variable that will hold the wanted query and will change together
+      //with the checkbox. watch hide completed and change the query variable
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideCompleted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          $scope.query = {};
+      });
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
+      };
 
       $scope.addTask = function(newTask){
         $scope.tasks.push( {
           text: newTask,
-          createdAt: new Date() }
+          createdAt: new Date(), //current time
+          ownder: Meteor.userId(), // _id of logged in user
+          username: Meteor.user().username //username of logged in user
+           }
           );
       };
+
+
 
     }]);
 }
