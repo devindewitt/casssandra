3f6278cf88c13d0d31788952f4bfd864ba866d36
ImmaDragon <devindewitt8@gmail.com>
2015-12-09 19:31:59 -0500

    fixed all

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -2,15 +2,13 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
 
-
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 
+  // This code only runs on the client
   angular.module('simple-todos',['angular-meteor', 'accounts.ui']);
 
-
-// cordova bitch
   function onReady() {
     angular.bootstrap(document, ['simple-todos']);
   }
@@ -19,18 +17,32 @@ if (Meteor.isClient) {
     angular.element(document).on('deviceready', onReady);
   else
     angular.element(document).ready(onReady);
-//
 
-  angular.module('simple-todos').controller('TodosListCtrl', ['$scope','$meteor',
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
 
-      //turn angular scope varaibles into meteor reactive variables
+      $scope.$meteorSubscribe('tasks');
+
       $scope.tasks = $meteor.collection(function() {
         return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
       });
 
-      //create a scope variable that will hold the wanted query and will change together
-      //with the checkbox. watch hide completed and change the query variable
+      $scope.addTask = function (newTask) {
+        $meteor.call('addTask', newTask);
+      };
+
+      $scope.deleteTask = function (task) {
+        $meteor.call('deleteTask', task._id);
+      };
+
+      $scope.setChecked = function (task) {
+        $meteor.call('setChecked', task._id, !task.checked);
+      };
+
+      $scope.setPrivate = function (task) {
+        $meteor.call('setPrivate', task._id, ! task.private);
+      };
+
       $scope.$watch('hideCompleted', function() {
         if ($scope.hideCompleted)
           $scope.query = {checked: {$ne: true}};
@@ -42,23 +54,60 @@ if (Meteor.isClient) {
         return Tasks.find({ checked: {$ne: true} }).count();
       };
 
-      $scope.addTask = function(newTask){
-        $scope.tasks.push( {
-          text: newTask,
-          createdAt: new Date(), //current time
-          ownder: Meteor.userId(), // _id of logged in user
-          username: Meteor.user().username //username of logged in user
-           }
-          );
-      };
-
-
-
     }]);
 }
 
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  }
+});
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish('tasks', function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
